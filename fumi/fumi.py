68e3219c0920ee76b0e84556f778dec4cbf4a623
fumi23 <fmr23s@gmail.com>
2019-06-15 12:56:57 +0900

    Support row-wise height

--- fumi/fumi.py
+++ fumi/fumi.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     h = [65, 65, 65];
 
     # 原点
-    origin_x = 340;
+    origin_x = 320;
     origin_y = 0;
 
     # 枠線を除去するマージンピクセル。縦横共通。
@@ -18,15 +18,14 @@ if __name__ == '__main__':
 
     img = Image.open(os.path.dirname(os.path.abspath(__file__)) + "/sample3.png", "r");
 
+    leftpos_x = origin_x;
     for pos_x in range(0, 3):
-        # 読み取るマスの左上位置 (x-coord)
-        leftpos_x = origin_x - (w + margin_w) * (pos_x + 1) - margin_w;
+        # 読み取るマスの左上位置
+        leftpos_x -= w + margin_w;
+        leftpos_y = origin_y + margin_h;
         for pos_y in range(0, 3):
-            # 読み取るマスの左上位置 (y-coord)
-            leftpos_y = origin_y + margin_h + (h[0] + margin_h) * pos_y;
-
             # 該当のマスをトリミング
-            crop = img.crop((leftpos_x, leftpos_y, leftpos_x + w, leftpos_y + h[0]));
+            crop = img.crop((leftpos_x, leftpos_y, leftpos_x + w, leftpos_y + h[pos_y]));
 
             # インストールしたtesseractコマンドのパス
             pytesseract.tesseract_cmd = "/usr/bin/tesseract";
@@ -35,3 +34,6 @@ if __name__ == '__main__':
             result = pytesseract.image_to_string(crop, config="-psm 10", lang="eng+jpn");
 
             print(result, end='\n' if pos_y == 2 else ',');
+
+            # 次に読み取るマスの左上位置 (y-coord)
+            leftpos_y += h[pos_y] + margin_h;
